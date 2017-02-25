8c569e5f5dbc59a595f123bc7c182db9afd51633
coldshower <john@johnchen.co>
2017-02-25 12:47:37 -0500

    started search Component

---
+++ frontend/components/search_bar/search_bar_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import component from './search_bar';
+
+const mapStateToProps = () => {
+  return {};
+}
+
+const mapDispatchToProps = () => {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(component);
