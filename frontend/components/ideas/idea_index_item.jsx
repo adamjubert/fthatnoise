46715cd8cdc2e89da6f97bdb7a175a2efc1bf861
Laura Cressman <lauracressman@mac.com>
2017-02-25 18:56:55 -0500

    start restyling idea index items

--- frontend/components/ideas/idea_index_item.jsx
+++ frontend/components/ideas/idea_index_item.jsx
@@ -23,16 +23,22 @@ const IdeaIndexItem = ({ idea, ideaType }) => {
 
   return (
   <div className="short-idea">
-      <h3 className="short-idea-header">
-        <Link to={ ideaLink }>
-          { idea.title } ({ idea.upvotes_count } activists)
-        </Link>
-      </h3>
+      <div className="short-idea-header">
+        <div>
+          <h3><Link to={ ideaLink }>
+            { idea.title }
+          </Link></h3>
+        </div>
+        <div className="short-idea-upvotes-count">
+          <h4>Followers</h4>
+          <h3>{ idea.upvotes_count }</h3>
+        </div>
+      </div>
       <div className="short-idea-body">
         <CategoriesString categories={ idea.categories } />
         { eventLogistics }
-        <p>Created by: { idea.creator.username }</p>
-        { idea.shortened_description }
+        <p><span>Created by: </span>{ idea.creator.username }</p>
+        <p><span>Details: </span>{ idea.shortened_description }</p>
         <UpvoteButtonsContainer idea={ idea } ideaType={ ideaType } />
       </div>
     </div>
